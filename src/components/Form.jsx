3cddfb4f02976ecc1a71d1c37e62a6a49a84f7ca
Trip4losky <agustinrallysport@gmail.com>
2022-11-07 20:44:07 +0000

    Add Hooks on Patient

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useState, useEffect } from "react"
 
 const Form = () => {
+  const [mascota, setMascota] = useState('');
+  const [propietario, setPropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [sintomas, setSintomas] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Enviando Formulario');
+  };
+
   return (
-    <div className='md:w-1/2 lg:w-2/5'>
+    <div className='md:w-1/2 lg:w-2/5 mx-5'>
 
       <h2 className='font-black text-3xl text-center'>Seguimiento Pacientes</h2>
 
@@ -11,30 +22,26 @@ const Form = () => {
         <span className='text-indigo-600 font-bold'>Administralos</span>
       </p>
 
-      <form className='bg-white shadow-md rounded-lg py-10 px-5 mb-10'>
+      <form className='bg-white shadow-md rounded-lg py-10 px-5 mb-10' onSubmit={handleSubmit}>
         <div className='mb-5'>
           <label htmlFor="mascota" className='block text-grey-700 uppercase font-bold'>Nombre Mascota</label>
-          <input id="mascota" className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' type='text' placeholder='Nombre de la Mascota'/>
+          <input id="mascota" className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' type='text' placeholder='Nombre de la Mascota' value={mascota} onChange={ (e) => setMascota(e.target.value)} />
         </div>
         <div className='mb-5'>
           <label htmlFor="propietario" className='block text-grey-700 uppercase font-bold'>Nombre Propietario</label>
-          <input id="propietario" className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' type='text' placeholder='Nombre del Propietario'/>
+          <input id="propietario" className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' type='text' placeholder='Nombre del Propietario'value={propietario} onChange={ (e) => setPropietario(e.target.value)}/>
         </div>
         <div className='mb-5'>
           <label htmlFor="email" className='block text-grey-700 uppercase font-bold'>Email</label>
-          <input id="email" className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' type='email' placeholder='Dirección de correo electrónico'/>
-        </div>
-        <div className='mb-5'>
-          <label htmlFor="email" className='block text-grey-700 uppercase font-bold'>Email</label>
-          <input id="email" className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' type='email' placeholder='Dirección de correo electrónico'/>
+          <input id="email" className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' type='email' placeholder='Dirección de correo electrónico'value={email} onChange={ (e) => setEmail(e.target.value)}/>
         </div>
         <div className='mb-5'>
           <label htmlFor="alta" className='block text-grey-700 uppercase font-bold'>Alta</label>
-          <input id="alta" className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' type='date' />
+          <input id="alta" className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' type='date' value={fecha} onChange={ (e) => setFecha(e.target.value)}/>
         </div>
         <div className='mb-5'>
           <label htmlFor="sintomas" className='block text-grey-700 uppercase font-bold'>Síntomas</label>
-          <textarea id="sintomas" className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' placeholder='Describe los Síntomas'/>
+          <textarea id="sintomas" className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' placeholder='Describe los Síntomas'value={sintomas} onChange={ (e) => setSintomas(e.target.value)}/>
         </div>
         <input type="submit" className='bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all' value="Agregar Paciente" />
       </form>
